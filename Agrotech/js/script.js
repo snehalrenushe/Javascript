352ee8c0b7bb2d89b5b339ca0872a1640ff14e1d
snehal renushe <snehalrenushe15@gmail.com>
2022-05-29 23:47:27 +0530

    Payment module added

--- Agrotech/js/script.js
+++ Agrotech/js/script.js
@@ -17,18 +17,39 @@
           prevEl: '.swiper-button-prev',
       },});
 
-      function navSlide(){
-        const nav = document.querySelector('.nav-links');
-        const navLinks = document.querySelectorAll('.nav-links li');
-        //Toggle nav
-        nav.classList.toggle('nav-active');
-    
-        //Animate links
-        navLinks.forEach((link,index) => {
-            link.style.animation = `navLinkFade 0.5s ease forwords ${index / 7 + 2}s`
-        });
-    } 
-    
-    function contact(){
-        alert("Thank you for contacting! We will get back to you shortly.");
-    }
+
+      //Burger Menu
+
+      const navSlide = ()=> 
+      {
+          const burger = document.querySelector('.burger');
+          const nav = document.querySelector('.nav-links');
+          const navLinks = document.querySelectorAll('.nav-links li');
+
+          //Toggle nav
+          burger.addEventListener('click',()=>
+          {
+            nav.classList.toggle('nav-active');   
+
+            //Animate links
+
+            navLinks.forEach((link,index) => {
+                if(link.style.animation)
+                {
+                    link.style.animation = ''
+                }
+                else
+                {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+
+                }
+
+            });
+            //Burger animation
+            
+            burger.classList.toggle('toggle');
+          });
+
+      }
+
+      navSlide();
